2ae086df167d7308f9912053a23fdcf17e0cefc6
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-02 18:36:02 -0600

    Created Joint constructor method & empty Skeleton constructor.

--- src/skeleton.py
+++ src/skeleton.py
@@ -1,9 +1,16 @@
 __author__ = 'Erik S Carlsten'
 
+from cgkit.cgtypes import mat4
+from cgkit import bvhimport
+
 
 class Skeleton:
-    pass
+    def __init__(self, bvh_importer):
+        pass
 
 
 class Joint:
-    pass
+    def __init__(self):
+        self.transform_parent = mat4(1.0)
+        self.transform = mat4(1.0)
+        self.children = []
