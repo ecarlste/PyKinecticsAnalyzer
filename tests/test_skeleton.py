d07a9c8a3936caa684bc622e2afeaf8cc1c6baf7
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-04 09:42:12 -0600

    Added tests package and shell for test_skeleton.py that tests Skeleton and Joint classes.

---
+++ tests/test_skeleton.py
@@ -0,0 +1,14 @@
+__author__ = 'Erik S Carlsten'
+
+
+from unittest import TestCase
+
+from src.skeleton import Joint, Skeleton
+
+
+class TestSkeleton(TestCase):
+    pass
+
+
+class TestJoint(TestCase):
+    pass
