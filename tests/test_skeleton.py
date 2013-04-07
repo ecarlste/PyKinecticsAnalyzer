97332cfcf804da6ad81491c908bdc4e85f5e4c50
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-07 13:06:44 -0600

    Added test_root_transform_parent, test_root_children_size, test_root_children_names test methods.

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -15,14 +15,32 @@ class TestSkeleton(TestCase):
         cls._reader = BVHReader('test_bvh_data.bvh')
         cls._reader.read()
 
-    def setUp(self):
-        self.skeleton = Skeleton(self._reader, 2)
-
     def test_root_name(self):
+        skeleton = Skeleton(self._reader, 2)
         name = 'Hips'
-        self.assertEqual(self.skeleton.root.name, name)
+        self.assertEqual(skeleton.root.name, name)
+
+    def test_root_transform_parent(self):
+        skeleton = Skeleton(self._reader, 2)
+        transform_parent = mat4().identity()
+        self.assertEqual(skeleton.root.transform_parent, transform_parent)
+
+    def test_root_children_size(self):
+        skeleton = Skeleton(self._reader, 2)
+        self.assertEqual(len(skeleton.root.children), 3)
+
+    def test_root_children_names(self):
+        skeleton = Skeleton(self._reader, 2)
+        names = ['Spine1', 'LeftUpLeg', 'RightUpLeg']
+        children_names = [
+            skeleton.root.children[0].name,
+            skeleton.root.children[1].name,
+            skeleton.root.children[2].name,
+        ]
+        self.assertEqual(children_names, names)
 
     def test_root_transform(self):
+        skeleton = Skeleton(self._reader, 2)
         transform = mat4(
             0, 1, 0, 0,
             0, 0, -1, 0,
@@ -30,7 +48,7 @@ class TestSkeleton(TestCase):
             0, 0, 0, 1
         )
 
-        TestUtility().assertAlmostEqual_mat4(transform, self.skeleton.root.transform, 12)
+        TestUtility().assertAlmostEqual_mat4(transform, skeleton.root.transform, 12)
 
 
 class TestJoint(TestCase):
