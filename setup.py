a5db0ea91e3ff4b377ff5f3286a57f7147d195cf
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-10 11:04:27 -0600

    Changed setup.py to use setuptools.

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-__author__ = 'Erik S Carlsten'
+from setuptools import setup, find_packages
+
+setup(
+    name='PyKineticsAnalyzer',
+    version='0.0.1',
+    author='Erik S. Carlsten',
+    author_email='erik@preactiontechnology.com',
+    packages = find_packages(), #['pykineticsanalyzer', 'pykineticsanalyzer.test'],
+    url='https://github.com/ecarlste/PyKinecticsAnalyzer/',
+    license='LICENSE.txt',
+    description='Skeleton Motion Kinetics Analysis.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "cgkit >= 2.0",
+        ],
+    )
